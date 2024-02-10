43e6fef41807e883cce1d6e306f57cca31b9c727
KianJanbozorgi <kian84.c16@gmail.com>
2024-02-11 01:01:25 +0330

    updated cleaning , cleaned sim details and fixed some issues

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -2,11 +2,17 @@ import numpy as np
 import pandas as pd
 import re
 
-class CameraDataProcessor:
-    def __init__(self, file_name):
-        self.df = pd.read_csv(file_name)
 
+class DataPreProcess:
+    def __init__(self, data_input):
+        # If data_input is a string, assume it's a filename. Otherwise, assume it's a DataFrame.
+        if isinstance(data_input, str):
+            self.df = pd.read_csv(data_input)
+        else:
+            self.df = data_input
 
+      # Convert to string and remove leading "~"
+    
     def process_main_camera_columns(self):
         camera_types = {
             'Main Camera_Single': 1,
@@ -48,45 +54,46 @@ class CameraDataProcessor:
         return cell
 
     def process_camera_resolutions(self):
-        self.df['Highest_maincam_res'] = self.df['Main Camera_Single'].combine_first(self.df['Main Camera_Dual']).combine_first(self.df['Main Camera_Triple']).combine_first(self.df['Main Camera_Quad']).combine_first(self.df['Main Camera_Dual or Triple']).combine_first(self.df['Main Camera_Penta']).combine_first(self.df['Main Camera_Five'])
+        self.df['Highest_maincam_res'] = self.df['Main Camera_Single'].combine_first(
+            self.df['Main Camera_Dual']).combine_first(self.df['Main Camera_Triple']).combine_first(
+            self.df['Main Camera_Quad']).combine_first(self.df['Main Camera_Dual or Triple']).combine_first(
+            self.df['Main Camera_Penta']).combine_first(self.df['Main Camera_Five'])
         self.df['Highest_maincam_res'] = self.df['Highest_maincam_res'].apply(self.get_highest_res)
-        self.df['Highest_selfiecam_res'] = self.df['Selfie camera_Single'].combine_first(self.df['Selfie camera_Dual']).combine_first(self.df['Selfie camera_Triple'])
+        self.df['Highest_selfiecam_res'] = self.df['Selfie camera_Single'].combine_first(
+            self.df['Selfie camera_Dual']).combine_first(self.df['Selfie camera_Triple'])
         self.df['Highest_selfiecam_res'] = self.df['Highest_selfiecam_res'].apply(self.get_highest_res)
-        
+
     def process_video_resolution(self):
         self.df['Main Camera_Video'] = self.df['Main Camera_Video'].apply(self.get_video_res)
         self.df['Selfie camera_Video'] = self.df['Selfie camera_Video'].apply(self.get_video_res)
-        
+
     def camera_features_listing(self):
         self.df['Main Camera_Features'] = self.df['Main Camera_Features'].apply(self.get_features_list)
         self.df['Selfie camera_Features'] = self.df['Selfie camera_Features'].apply(self.get_features_list)
-        
 
-    def drop_old_columns(self):
-        self.df = self.df.drop(columns=["Main Camera_Single","Main Camera_Dual","Main Camera_Triple","Main Camera_Quad","Main Camera_Dual or Triple","Main Camera_Penta","Main Camera_Five", "Selfie camera_Single", "Selfie camera_Dual", "Selfie camera_Triple", "Selfie camera_", "Main Camera", "Selfie camera", "Main Camera_"])
-    def demintions_process(self):    
+    def demintions_process(self):
         length = []
         width = []
         height = []
         volume = []
-        for x in list(df['Body_Dimensions']):
+        for x in list(self.df['Body_Dimensions']):
             try:
-                m = re.findall('(\d+(?:\.\d+)?)' , x)
+                m = re.findall('(\d+(?:\.\d+)?)', x)
                 length.append(float(m[0]))
             except:
                 length.append(np.nan)
             try:
-                m = re.findall('(\d+(?:\.\d+)?)' , x)
+                m = re.findall('(\d+(?:\.\d+)?)', x)
                 width.append(float(m[1]))
             except:
                 width.append(np.nan)
             try:
-                m = re.findall('(\d+(?:\.\d+)?)' , x)
+                m = re.findall('(\d+(?:\.\d+)?)', x)
                 height.append(float(m[2]))
             except:
                 height.append(np.nan)
             try:
-                m = re.findall('(\d+(?:\.\d+)?)' , x)
+                m = re.findall('(\d+(?:\.\d+)?)', x)
                 volume.append(float(m[0]) * float(m[1]) * float(m[2]))
             except:
                 volume.append(np.nan)
@@ -97,44 +104,44 @@ class CameraDataProcessor:
 
     def weight_process(self):
         weight = []
-        for x in list(df['Body_Weight']):
+        for x in list(self.df['Body_Weight']):
             try:
-                m = re.findall('(\d+(?:\.\d+)?)' , x)
+                m = re.findall('(\d+(?:\.\d+)?)', x)
                 weight.append(float(m[0]))
             except:
                 m = np.nan
                 weight.append(m)
-        df['weight'] = weight
-    
+        self.df['weight'] = weight
+
     def network_tech_process(self):
-        df['Network_2G bands'].fillna(0 , inplace=True)
-        df['Network_3G bands'].fillna(0 , inplace=True)
-        df['Network_4G bands'].fillna(0 , inplace=True)
-        df['Network_5G bands'].fillna(0 , inplace=True)
+        self.df['Network_2G bands'].fillna(0, inplace=True)
+        self.df['Network_3G bands'].fillna(0, inplace=True)
+        self.df['Network_4G bands'].fillna(0, inplace=True)
+        self.df['Network_5G bands'].fillna(0, inplace=True)
         net_2g = []
-        for x in list(df['Network_2G bands']):
-            if x == 0 :
+        for x in list(self.df['Network_2G bands']):
+            if x == 0:
                 net_2g.append(0)
             else:
                 net_2g.append(1)
         self.df['2G'] = net_2g
         net_3g = []
-        for x in list(df['Network_3G bands']):
-            if x == 0 :
+        for x in list(self.df['Network_3G bands']):
+            if x == 0:
                 net_3g.append(0)
             else:
                 net_3g.append(1)
         self.df['3G'] = net_3g
         net_4g = []
-        for x in list(df['Network_4G bands']):
-            if x == 0 :
+        for x in list(self.df['Network_4G bands']):
+            if x == 0:
                 net_4g.append(0)
             else:
                 net_4g.append(1)
         self.df['4G'] = net_4g
         net_5g = []
-        for x in list(df['Network_5G bands']):
-            if x == 0 :
+        for x in list(self.df['Network_5G bands']):
+            if x == 0:
                 net_5g.append(0)
             else:
                 net_5g.append(1)
@@ -142,9 +149,9 @@ class CameraDataProcessor:
 
     def battery_capacity_process(self):
         battery_capacity = []
-        for x in list(df['Battery_Type']):
+        for x in list(self.df['Battery_Type']):
             try:
-                m = re.findall('(\d+)' , x)
+                m = re.findall('(\d+)', x)
                 battery_capacity.append(int(m[0]))
             except:
                 m = np.nan
@@ -153,7 +160,7 @@ class CameraDataProcessor:
 
     def sensors_process(self):
         sensors = []
-        for x in list(df['Features_Sensors']):
+        for x in list(self.df['Features_Sensors']):
             try:
                 m = x.split(',')
                 sensors.append(m)
@@ -161,21 +168,256 @@ class CameraDataProcessor:
                 sensors.append(np.nan)
         self.df['Sensors'] = sensors
 
+    def extract_display_characteristics(self):
+        self.df['Display_Size_Inch'] = self.df['Display_Size'].apply(
+            lambda x: self._extract_with_regex(x, r'(\d+\.?\d*) inches')
+        )
+        self.df['Display_Size_Cm'] = self.df['Display_Size'].apply(
+            lambda x: self._extract_with_regex(x, r'(\d+\.?\d*) cm2')
+        )
+        self.df['Screen_To_Body_Ratio'] = self.df['Display_Size'].apply(
+            lambda x: self._extract_with_regex(x, r'(~\d+\.?\d*%)')
+        )
+        def remove_tilde_percent(value):
+            try:
+                return float(str(value).replace("~", "").replace("%", ""))
+            except:
+                return (str(value).replace("~", "").replace("%", ""))
+        self.df['Screen_To_Body_Ratio']= self.df['Screen_To_Body_Ratio'].apply(remove_tilde_percent)
+        self.df['Screen_To_Body_Ratio'].value_counts()
+    def SIM_process(self):
+        type_sim = []
+        count = []
+        for x in list(self.df['Body_SIM']):
+            try:
+                x_lower = x.lower()
+                nano = x_lower.find('nano')
+                mini = x_lower.find('mini')
+                micro = x_lower.find('micro')
+                if nano > -1 :
+                    type_sim.append('nano')
+                elif micro > -1 :
+                    type_sim.append('micro')
+                elif mini > -1 :
+                    type_sim.append('mini')
+                else:
+                    type_sim.append('unkomwn')
+                dual = x_lower.find('dual')
+                single = x_lower.find('single')
+                if (single > -1) & (dual > -1):
+                    count.append('both')
+                elif dual > -1:
+                    count.append('dual')
+                else:
+                    count.append('single')
+            except:
+                type_sim.append(np.nan)
+                count.append(np.nan)
+        self.df['SIM_type'] = type_sim
+        self.df['SIM_count'] = count
+    
+    def extract_resolution_details(self):
+        self.df['Resolution_Pixels'] = self.df['Display_Resolution'].apply(
+            lambda x: self._extract_with_regex(x, r'(\d+ x \d+) pixels')
+        )
+        self.df['Resolution_Ratio'] = self.df['Display_Resolution'].apply(
+            lambda x: self._extract_with_regex(x, r'(\d+:\d+) ratio')
+        )
+        self.df['PPI_Density'] = self.df['Display_Resolution'].apply(
+            lambda x: self._extract_with_regex(x, r'(~?\d+) ppi')
+        )
+        def remove_tilde(value):
+            return str(value).lstrip("~")
+        self.df['PPI_Density'] = self.df['PPI_Density'].apply(remove_tilde)
+        self.df['PPI_Density'].value_counts()
+
+    def extract_base_os(self):
+
+        self.df['base_os'] = self.df['Platform_OS'].apply(
+            lambda x: self._extract_os_from_first_space(x)
+        )
+
+    def _extract_os_from_first_space(self, os_string):
+
+        return os_string.split(' ', 1)[0] if isinstance(os_string, str) else os_string
+
+    def extract_os_version(self):
+
+        self.df['OS_Version'] = self.df['Platform_OS'].apply(
+            lambda x: self._extract_version_number(x)
+        )
+
+    def _extract_version_number(self, os_string):
+
+        if isinstance(os_string, str):
+            # Adjusted regex pattern to capture only the numeric part before any spaces or additional text
+            match = re.search(r'\b(?:Android|iOS|Windows)\b\s([\d]+)', os_string)
+            if match:
+                return match.group(1)  # Return only the numeric part of the version
+        return "Unknown"
+
+    def extract_chipset_manufacturer(self):
+        manufacturers = ["Qualcomm", "Mediatek", "Apple", "Samsung", "Exynos", "Intel Atom"]
+        self.df['Chipset_Manufacturer'] = self.df['Platform_Chipset'].apply(
+            lambda x: next((manufacturer for manufacturer in manufacturers if manufacturer in str(x)), "Other")
+        )
+
+    def extract_cpu_core_count(self):
+        self.df['CPU_Core_Count'] = self.df['Platform_CPU'].apply(
+            lambda x: self._extract_with_regex(x, r'(Dual|Quad|Hexa|Octa|Deca)-core')
+        )
+
+    def preprocess_memory_card_slot(self):
+
+        self.df['Card_Slot_Type'] = self.df['Memory_Card slot'].apply(self._preprocess_memory_card_slot_value)
+
+    def _preprocess_memory_card_slot_value(self, value):
+
+        if pd.isna(value):
+            return None  # Handle missing or null values
+        value_str = str(value).lower()  # Standardize the string for comparison
+
+        # Define keywords that imply card slot support
+        keywords = ['microsd', 'sd', 'mmc', 'minisd', 'nanomemory']
+
+        # Check for explicit "no" mention
+        if 'no' in value_str:
+            return 'No'
+
+        # Check and return the specific keyword found in the value
+        for keyword in keywords:
+            if keyword in value_str:
+                return keyword.capitalize()  # Return the keyword with the first letter capitalized for consistency
+
+        # If "yes" is mentioned but no specific type is identified
+        if 'yes' in value_str:
+            return 'Yes'
+
+        # Return 'Unknown' if none of the conditions are met
+        return 'Unknown'
+
+    def expand_memory_configurations(self, column_name='Memory_Internal'):
+        # Temporary container for the new rows
+        temp_df_list = []
+
+        for _, row in self.df.iterrows():
+            configs = str(row[column_name]).split(', ') if pd.notnull(row[column_name]) else [None]
+            for config in configs:
+                new_row = row.copy()
+                new_row[column_name] = config
+                temp_df_list.append(new_row)
+
+        self.df = pd.DataFrame(temp_df_list).reset_index(drop=True)
+
+    def convert_storage_ram(self):
+        # Convert storage and RAM sizes
+        self.df['Internal_Storage_GB'] = self.df['Memory_Internal'].str.extract(r'(\d+)\s*GB',
+                                                                                expand=False).astype(
+            float)
+        storage_in_mb = self.df['Memory_Internal'].str.extract(r'(\d+)\s*MB', expand=False).astype(float) / 1024
+        self.df['Internal_Storage_GB'].fillna(storage_in_mb, inplace=True)
+
+        self.df['RAM_GB'] = self.df['Memory_Internal'].str.extract(r'(\d+)\s*GB RAM',
+                                                                   expand=False).astype(float)
+        ram_in_mb = self.df['Memory_Internal'].str.extract(r'(\d+)\s*MB RAM', expand=False).astype(float) / 1024
+        self.df['RAM_GB'].fillna(ram_in_mb, inplace=True)
+
+    def clean_and_extract_price(self):
+        def clean_price(price):
+            pattern = re.compile(r'([\$\€\£])?\s*(\d+(?:\.\d+)?)')
+            matches = pattern.findall(str(price))
+            if matches:
+                currency_symbols = {'$': 'USD', '€': 'EUR', '£': 'GBP'}
+                currency, amount = matches[0]
+                currency = currency_symbols.get(currency, 'Unknown')
+                amount = float(amount)
+                return amount
+            return 'Unknown', None
+
+        self.df['Cleaned_Price'] = pd.DataFrame(
+            self.df['Misc_Price'].apply(clean_price).tolist(), index=self.df.index
+        )
+
+    def _extract_with_regex(self, text, pattern):
+        if pd.isna(text):
+            return None
+        match = re.findall(pattern, str(text))
+        return match[0] if match else None
+
+    def drop_old_columns(self):
+        self.df = self.df.drop(
+            columns=[
+                    'Network_Technology','Network_','Network_Speed','Display_Type','Memory_','Main Camera_Features', 
+                    'Main Camera_Video', 'Selfie camera_Video',
+                    'Sound_Loudspeaker', 'Sound_3.5mm jack', 'Comms_WLAN',
+                    'Comms_Bluetooth', 'Comms_Positioning', 'Comms_NFC', 'Comms_Radio',
+                    'Comms_USB', 'Misc_Colors',
+                    "Main Camera_Single", "Main Camera_Dual", "Main Camera_Triple", "Main Camera_Quad",
+                     "Main Camera_Dual or Triple", "Main Camera_Penta", "Main Camera_Five", "Selfie camera_Single",
+                     "Selfie camera_Dual", "Selfie camera_Triple", "Selfie camera_", "Main Camera", "Selfie camera",
+                     "Main Camera_",
+                     'Display_Size', 'Display_Resolution', 'Platform_OS', 'Platform_Chipset', 'Platform_CPU',
+                     'Platform_GPU',
+                     'Memory_Internal', 'Memory_Card slot', 'Misc_Models', 'Misc_Price',
+                     'Body_Dimensions', 'Body_Weight', 'Network_2G bands', 'Network_3G bands', 'Network_4G bands',
+                     'Network_5G bands', 'Battery_Type', 'Features_Sensors',
+                     'Body_Build', 'Selfie camera_Features', 'Display_Protection',
+                     'Battery_Charging', 'Display_', 'Misc_SAR', 'Network_GPRS',
+                     'Network_EDGE', 'Body_', 'Comms_Infrared port',
+                     'Battery_Stand-by', 'Battery_Talk time', 'Features_', 'Sound_',
+                     'Battery_Music play', 'Platform', 'Memory_Phonebook', 'Memory_Call records',
+                     'Features_Messaging', 'Features_Games', 'Features_Java', 'Misc_SAR EU', 'Body_Keyboard',
+                     'Features_Browser', 'Sound_Alert types', 'Features_Clock', 'Features_Alarm', 'Features_Languages'])
+
+    def drop_columns(self, columns_to_drop):
+
+        self.df.drop(columns=columns_to_drop, inplace=True, errors='ignore')
+
+    def save_processed_data(self, file_name='processed_data.csv'):
+        # Save the processed DataFrame to a CSV file
+        self.df.to_csv(file_name, index=False)
+    def final_adjustments(self):
+        self.df['Launch_Announced'] = self.df['Launch_Announced'].replace('Not announced yet', np.nan)
+        self.df['year'] = self.df['Launch_Announced'].str.extract(r'(\d{4})')
+        self.df = self.df[self.df['year'].astype(float) > 2010]
+        self.df['Resolution_Pixels'] = self.df['Resolution_Pixels'].fillna('0 x 0')
+        self.df['Resolution_Pixels'] = self.df['Resolution_Pixels'].astype(str)
+        self.df['Resolution_Pixels'] = self.df['Resolution_Pixels'].apply(lambda x: int(x.split(' x ')[0]) * int(x.split(' x ')[1]) if x != 'nan' else np.nan)
+    def year_to_int(self):
+        int_year = []
+        for x in list(self.df['year']):
+            m = int(x)
+            int_year.append(m)
+        self.df['year'] = int_year
+
     def process(self):
         self.process_main_camera_columns()
         self.process_selfie_camera_columns()
         self.process_camera_resolutions()
         self.process_video_resolution()
         self.camera_features_listing()
-        self.drop_old_columns()
         self.weight_process()
         self.demintions_process()
         self.network_tech_process()
         self.battery_capacity_process()
         self.sensors_process()
+        self.SIM_process()
+        self.extract_display_characteristics()
+        self.extract_resolution_details()
+        self.extract_base_os()
+        self.extract_os_version()
+        self.extract_chipset_manufacturer()
+        self.extract_cpu_core_count()
+        self.preprocess_memory_card_slot()
+        self.expand_memory_configurations()
+        self.convert_storage_ram()
+        self.clean_and_extract_price()
+        self.drop_old_columns()
+        self.final_adjustments()
+        self.year_to_int()
         return self.df
-    
-    
-   
-processor = CameraDataProcessor('flattened_data.csv')
-df = processor.process()
+
+
+data_processor = DataPreProcess('flattened_data.csv')
+data_processor.process()
+data_processor.save_processed_data('processed_data.csv')
